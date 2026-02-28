34e10b9cef126dca4fb0ffecd1e2b599bd599492
omkarp-21 <97591115+omkarp-21@users.noreply.github.com>
2026-02-28 16:34:36 +0530

    feat(system): initialize securevault root, system, and users directories

---
+++ src/com/securevault/Main.java
@@ -0,0 +1,7 @@
+import system.SystemInitializer;
+
+void main(String[] args) {
+    System.out.println("Secure Vault");
+    SystemInitializer systemInitializer = new SystemInitializer();
+    systemInitializer.initialize();
+}
